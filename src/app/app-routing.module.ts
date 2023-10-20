c68a8ce167e85e73ae48de25e2cb8613de3a6329
Marlon-Schafranski <marlonschafranski963@gmail.com>
2023-10-20 14:48:26 -0300

    projeto em construçao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
+
+
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./view/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'detalhar',
+    loadChildren: () => import('./view/detalhar/detalhar.module').then( m => m.DetalharPageModule)
+  },
+  {
+    path: 'cadastrar',
+    loadChildren: () => import('./view/cadastrar/cadastrar.module').then( m => m.CadastrarPageModule)
+  },
+
+
+
+
 ];
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAzQLvV5yNP8dcGkQnTZaaArve0VoP1ZaQ",
+    authDomain: "computadores-d5d4e.firebaseapp.com",
+    projectId: "computadores-d5d4e",
+    storageBucket: "computadores-d5d4e.appspot.com",
+    messagingSenderId: "1030132347863",
+    appId: "1:1030132347863:web:8d574c437712a682621192",
+    measurementId: "G-FLYLP7M52J"
+}
+}
